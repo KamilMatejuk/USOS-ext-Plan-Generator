22a8a7981696e0479712b67e910221e1cf1005bc
KamilMatejuk <kamilmatejuk@gmail.com>
2024-10-06 19:52:27 +0200

    details & summary first style

--- add_options.js
+++ add_options.js
@@ -21,21 +21,22 @@ function _generateOptions() {
 function renderTopUI() {
     const parent = document.querySelector('div.usos-ui');
     const container = document.createElement('details');
+    container.style.border = '1px solid HSL(52, 90%, 58%)';
+    container.style.background = '#FCF5C7';
+    container.style.borderRadius = '10px';
     // summary
     const summary = document.createElement('summary');
     summary.innerText = 'Generator Planu USOS'
     summary.style.padding = '10px';
-    summary.style.border = '1px solid HSL(52, 90%, 58%)';
+    summary.style.border = '2px solid HSL(52, 90%, 58%)';
     summary.style.background = '#FCF5C7';
     summary.style.borderRadius = '10px';
     summary.style.cursor = 'pointer';
     container.appendChild(summary);
     // contents
     const contents = document.createElement('div');
-    contents.style.border = '1px solid HSL(52, 90%, 58%)';
-    contents.style.background = '#FCF5C7';
-    contents.style.borderRadius = '10px';
     contents.style.padding = '10px';
+
     contents.appendChild(_generateOptions());
     container.appendChild(contents);
     parent.children[1].insertAdjacentElement('afterend', container);
