65410326d440d0a73f111cf636b0a6c3248ce774
KamilMatejuk <kamilmatejuk@gmail.com>
2024-10-17 16:47:28 +0200

    load only on plan page

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-FULL_PLAN = readPlan(); // from read_plan.js
-// console.table(FULL_PLAN);
+const urlParams = new URLSearchParams(window.location.search);
+const action = urlParams.get('_action');
+if (action == 'home/plany/pokaz') {
 
-SELECTED = [];
-loadFromLocalStorage();
+    FULL_PLAN = readPlan(); // from read_plan.js
+    // console.table(FULL_PLAN);
+    
+    SELECTED = [];
+    loadFromLocalStorage();
+    
+    renderTopUI(); // from add_options.js
+    editTimetableUI(); // from edit_timetable.js
 
-renderTopUI(); // from add_options.js
-editTimetableUI(); // from edit_timetable.js
+}
